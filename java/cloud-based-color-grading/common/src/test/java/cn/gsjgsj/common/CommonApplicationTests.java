6ac1a8b67dac79615235a3db3c08e19edb9f823a
gaoshijie <2608917284@qq.com>
2026-01-11 22:31:14 +0800

    init

---
+++ java/cloud-based-color-grading/common/src/test/java/cn/gsjgsj/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gsjgsj.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
